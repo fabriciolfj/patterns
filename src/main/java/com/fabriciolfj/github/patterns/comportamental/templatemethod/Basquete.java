bf8aaae3afad7c34a384cd9be0efae310a81f29e
fabricio <fabricio.jacob>
2020-12-28 08:11:13 -0300

    add

---
+++ src/main/java/com/fabriciolfj/github/patterns/comportamental/templatemethod/Basquete.java
@@ -0,0 +1,8 @@
+package com.fabriciolfj.github.patterns.comportamental.templatemethod;
+
+public class Basquete extends Game {
+    @Override
+    protected void jogar() {
+        System.out.println("Jogo basquete");
+    }
+}
