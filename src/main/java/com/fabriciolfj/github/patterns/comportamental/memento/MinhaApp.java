bfef302a18c015dd016b1a3acc0d9cd460fde7b5
fabricio <fabricio.jacob>
2021-01-08 08:03:58 -0300

    add

---
+++ src/main/java/com/fabriciolfj/github/patterns/comportamental/memento/MinhaApp.java
@@ -0,0 +1,27 @@
+package com.fabriciolfj.github.patterns.comportamental.memento;
+
+public class MinhaApp {
+
+	public static void main(String[] args) {
+		//Cria objeto vendedor
+		Vendedor vendedor = new Vendedor("Jo�o", 10000.0F);
+		
+		System.out.println(vendedor.getNome() + " - Total de vendas: " + vendedor.getTotalVendas() + "\n");
+		
+		//Salva estado interno
+		VendasMemory memory = new VendasMemory();
+		memory.setMemento(vendedor.createMemento());
+		
+		//Altera os valores dos atributos do objeto vendedor
+		vendedor.setNome("Pedro");
+		vendedor.setTotalVendas(50000.0F);
+		
+		System.out.println(vendedor.getNome() + " - Total de vendas: " + vendedor.getTotalVendas() + "\n");
+		
+		//Restaura memento
+		vendedor.retoreMemeneto(memory.getMemento());
+		
+		System.out.println(vendedor.getNome() + " - Total de vendas: " + vendedor.getTotalVendas() + "\n");
+	}
+
+}
